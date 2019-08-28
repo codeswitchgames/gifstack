43ab5a26bddff89da10dd480bcdb5989a1585722
DeBray Carpenter <debraycarpenter@DeBrays-MacBook-Pro.local>
2019-08-27 19:46:07 -0700

    initial repo setup

---
+++ build/precache-manifest.c4ead3f8ba794a3cdde8b53bee44d448.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e56738bcbbd2ab1d7e40c963633882c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81200fba2e958a63ca54",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "c130b1769f0dc6aea5eb",
+    "url": "/static/css/main.cede614b.chunk.css"
+  },
+  {
+    "revision": "81200fba2e958a63ca54",
+    "url": "/static/js/2.bbf2dfac.chunk.js"
+  },
+  {
+    "revision": "c130b1769f0dc6aea5eb",
+    "url": "/static/js/main.cadea4dc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
